b89eb7268f82409d9dd3f082c0e90a6d9018e0ad
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2023-09-03 21:11:19 +0530

    Updated File.java

--- File.java
+++ File.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
-class Main{
-  public static void main(String[] args){
+class File1{
+  void copy(){
   Scanner sc=new Scanner(System.in);
     try{
     File f=new File("new.txt");
+    File f2=new File("out txt");
     boolean x=f.createNewFile();
-      if(x){
+    boolean y=f2.createNewFile();
+      if(x&&y){
         System.out.println("New file created.");
       }
       else{
@@ -15,23 +17,29 @@ class Main{
       System.out.println("Enter the contents to add in file: ");
       String s=sc.nextLine();
       FileWriter w=new FileWriter("new.txt");
+      FileWriter w1=new FileWriter("out.txt");
+      FileReader r=new FileReader("new.txt");
       w.write(s);
-      System.out.println("Data has been succesfully written to file");
       w.close();
-      int a;
-      System.out.println("Enter 1 to display the contents in file.");
-      a=sc.nextInt();
-      if(a==1){
-        char[] ch=new char[100];
-        FileReader r=new FileReader("new.txt");
-        r.read(ch);
-        System.out.println(ch);
-        r.close();
+      String str="";
+      int i;
+      System.out.println("Data has been succesfully written to file");
+      while((i=r.read())!=-1){
+        str+=(char)i;
       }
+      r.close();
+      w1.write(str);
+      w1.close();
     }
     catch(Exception e)
       {
         System.out.println(e);
       }
+  }
+}
+class Main{
+  public static void main(String[] args){
+    File1 x=new File1();
+    x.copy();
   }
 }
